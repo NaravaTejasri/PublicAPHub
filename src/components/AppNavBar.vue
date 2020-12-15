a99b4495fdaa9aad30677f6b9af70c026f6ad2a3
TejasriNarava <tejasrisony17@gmail.com>
2020-12-15 21:07:38 +0100

    Formatted spaces, empty lines and added comments in css.

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -6,7 +6,6 @@
         <a class='navbar-link'>Public API</a>
       </b-navbar-brand>
       <b-navbar-toggle target='nav-collapse'></b-navbar-toggle>
-
       <b-collapse id='nav-collapse' is-nav>
         <b-navbar-nav id='main-nav' class='ml-auto'>
           <b-nav-item class='nav-item' :to='{ name: "Home" }' position='relative'>
